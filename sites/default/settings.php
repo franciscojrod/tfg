696425f2e7a6a3766d3033944e252cd62603f227
Fran <emperador.palpatine@gmail.com>
2020-05-23 13:02:33 +0200

    profile folder and sites

---
+++ sites/default/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+// #ddev-generated: Automatically generated Drupal settings file.
+if (file_exists($app_root . '/' . $site_path . '/settings.ddev.php')) {
+  include $app_root . '/' . $site_path . '/settings.ddev.php';
+}
+
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
